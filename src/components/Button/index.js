c82c9fde7e54fd89dda55d5d768348609c454548
frrodriguez <frrodriguez@esei.uvigo.es>
2017-07-09 19:31:01 +0200

    Agregar componentes genericos

---
+++ src/components/Button/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Button from 'react-bootstrap/lib/Button';
+
+export default ({text}) => (
+   <Button bsStyle="primary" >{text}</Button>
+)
---
+++ src/components/MySpinner/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Loader from 'halogen/ClipLoader'
+
+export default (props) => {
+   return (<Loader {...props}/>)
+}
